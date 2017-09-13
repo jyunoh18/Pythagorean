27268d706578ac766e2b7a1bee636642c9f08b67
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-09-13 09:11:55 -1000

    Update pythagorean.js

--- pythagorean.js
+++ pythagorean.js
@@ -1,9 +1,9 @@
 HTML part
 
 <p>A:</p>
-<input type="text" id="A"></input>
+<input type="number" id="A"></input>
 <p>B:</p>
-<input type="text" id="B"></input>
+<input type="number" id="B"></input>
 
 
 <p id="C">C</p>
@@ -23,9 +23,7 @@ var A = document.getElementById('A').value;
 var B = document.getElementById('B').value;
 
 
-var c = Math.sqrt(Math.pow([A],2)+Math.pow([B],2));
+var c = Math.sqrt(Math.pow([A],2)+Math.pow([B],2));   / .tofixed(#) shows the maximum decimal places depending on the value #
 document.getElementById('C').innerHTML = "C: "+c;
-
-
 
 };
